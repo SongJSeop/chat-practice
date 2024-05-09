156bfb20904b046a2ca7b92eeee470d2a1ba58fe
송준섭 <wnstjq7979@gmail.com>
2024-05-09 15:48:12 +0900

    [FE] ChatRoomInnerDiv 컴포넌트 추가

--- client/src/components/ChatRoom.tsx
+++ client/src/components/ChatRoom.tsx
@@ -1,15 +1,28 @@
 import styled from "styled-components";
 
 export const ChatRoom = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: var(--chat-room-color);
-    height: 50px;
-    border: 1px solid var(--background-color);
-    border-radius: 5px;
-    
-    &:hover {
-        background-color: var(--chat-room-hover-color);
-    }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--chat-room-color);
+  height: 50px;
+  border: 1px solid var(--background-color);
+
+  &:hover {
+    background-color: var(--chat-room-hover-color);
+  }
+`;
+
+export const ChatRoomInnerDiv = styled.div<{
+  widthPercent: number;
+  textAlign: string;
+}>`
+  display: flex;
+  justify-content: ${(props) => props.textAlign};
+  align-items: center;
+  width: ${(props) => props.widthPercent}%;
+  height: 100%;
+  font-size: 1.5rem;
+  font-family: "Roboto", sans-serif;
+  border: 1px solid var(--background-color);
 `;
