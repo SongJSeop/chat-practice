eb378b75b6cdde353ff26e040f0ca03a55cf3fab
송준섭 <wnstjq7979@gmail.com>
2024-05-09 15:26:13 +0900

    [FE] yarn.lock 재생성

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
-import {ChatRoom} from "../components";
+import {ChatRoom} from "components";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
