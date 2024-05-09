98ddcf4421833657fdd609f382da62953fd0ce13
송준섭 <wnstjq7979@gmail.com>
2024-05-09 16:30:50 +0900

    [FE] Header에 유저 생성, 방 생성 버튼 추가

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,3 +1,11 @@
+import { useUserStore } from "../../stores/useUserStore.ts";
+
 export default function Header() {
-  return <header>Header</header>;
+  const { user, setUser } = useUserStore();
+  return (
+    <header>
+      {user ? <span>회원 id: {user.id}</span> : <button>유저 생성</button>}
+      {user ? <button>방 생성</button> : null}
+    </header>
+  );
 }
