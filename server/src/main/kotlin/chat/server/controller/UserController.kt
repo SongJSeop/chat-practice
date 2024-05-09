c3b6bb023bb4a1c5ba45ba5f9065501625a26d5c
송준섭 <wnstjq7979@gmail.com>
2024-05-09 16:46:42 +0900

    [BE] UserController Cors 설정

--- server/src/main/kotlin/chat/server/controller/UserController.kt
+++ server/src/main/kotlin/chat/server/controller/UserController.kt
@@ -2,12 +2,14 @@ package chat.server.controller
 
 import chat.server.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = ["http://localhost:5173"])
 class UserController @Autowired constructor(
     private val userService: UserService
 ) {
