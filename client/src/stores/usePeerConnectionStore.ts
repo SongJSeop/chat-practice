3480be3d80801e54c86165a4304c647999306b45
송준섭 <wnstjq7979@gmail.com>
2024-05-14 11:33:14 +0900

    [FE] usePeerConnectionStore.ts 추가

---
+++ client/src/stores/usePeerConnectionStore.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+
+export interface PeerConnection {
+  peerUserId: string;
+  connection: RTCPeerConnection;
+}
+
+interface PeerConnectionState {
+  peerConnections: PeerConnection[];
+  setPeerConnections: (peerConnections: PeerConnection[]) => void;
+  addPeerConnection: (peerConnection: PeerConnection) => void;
+}
+
+export const usePeerConnectionStore = create<PeerConnectionState>((set) => ({
+  peerConnections: [],
+  setPeerConnections: (peerConnections) =>
+    set((state) => ({ ...state, peerConnections })),
+  addPeerConnection: (peerConnection) =>
+    set((state) => ({
+      ...state,
+      peerConnections: [...state.peerConnections, peerConnection],
+    })),
+}));
